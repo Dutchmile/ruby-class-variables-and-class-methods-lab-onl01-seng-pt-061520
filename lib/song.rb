d7ef7528bef49831ccd0cdd8df992923118170a1
Christopher Dutcher <cadutcher@icloud.com>
2020-07-10 16:08:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,10 @@ attr_accessor :name, :artist, :genre
   def self.genre_count
     genre_count_hash = {}
     @@genres.each do |genre|
+      if
+         genre_count_hash.key?(genre) = false
+        genre_count_hash[genre] = 1
+      else
       genre_count_hash[genre] += 1
     end
     end
