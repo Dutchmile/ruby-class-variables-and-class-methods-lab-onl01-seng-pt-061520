2c4eab71ba306dbe645184b230923155bf8bcc6a
Christopher Dutcher <cadutcher@icloud.com>
2020-07-10 13:58:41 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << @artist
+    @genre = genre
+    @@genres << @genre
+    @@song_count += 1
+  end
+
+  def self.count
+    @@song_count
+  end
+  
+end
